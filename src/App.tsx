92d85ca39f0a7a0e0f61e60ebb4e446d6a0ce7d2
DaviCorreia <davicorreia21@gmail.com>
2020-11-02 00:33:48 -0300

    Criação do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Davi</h1>
+    </div>
+  );
+}
+
+export default App;
