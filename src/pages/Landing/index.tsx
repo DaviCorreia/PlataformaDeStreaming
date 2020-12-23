1272eaf28c6f50706fff7aa05d7285f26b477d5a
DaviCorreia <davicorreia21@gmail.com>
2020-12-22 23:53:27 -0300

    Finalização do layout responsivo

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
+
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing(){
     return(
@@ -22,12 +25,12 @@ function Landing(){
             </a>
 
             <a href=""className="give-classes" >
-                <img src={giveClassesIcon} alt="Estudar"/>
-                Estudar
+                <img src={giveClassesIcon} alt="Dar aulas"/>
+                Dar aulas
             </a>
         </div>
 
-        <span className="total-conections">
+        <span className="total-connections">
             Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
         </div>
