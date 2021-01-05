642737155591dae4eb868e10000f6bde6f31ac5d
DaviCorreia <davicorreia21@gmail.com>
2021-01-04 22:20:16 -0300

    configurando server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/users',(request,response)=>{
+
+    const users = [
+        {name:'Davi', age: 27},
+        {name:'Davi', age: 27},
+        {name:'Davi', age: 27},
+        {name:'Davi', age: 27},
+    ];
+    return response.json(users);
+    //console.log('Acessou a rota')
+});
+
+app.listen(3333);
